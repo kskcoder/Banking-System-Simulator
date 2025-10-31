ca02de170654408e57bcef16b53b3a0917052d7e
kskcoder <tejaskashid21@gmail.com>
2025-10-31 21:41:46 +0530

    - Implemented api gateway for banking simulator.

---
+++ bankapigateway/src/test/java/com/tejas/bankapigateway/BankapigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tejas.bankapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankapigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
