d78da7d800717c006f64a82338017b12e5bd6d5a
kskcoder <tejaskashid21@gmail.com>
2025-11-01 23:55:19 +0530

    - Added config server project to the repo.

---
+++ configserverservice/src/main/java/com/tejas/configserverservice/ConfigserverserviceApplication.java
@@ -0,0 +1,13 @@
+package com.tejas.configserverservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigserverserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserverserviceApplication.class, args);
+	}
+
+}
