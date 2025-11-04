48f931b2bdf0ea1709dbbb918e209af3ba3c648c
kskcoder <tejaskashid21@gmail.com>
2025-11-05 02:21:45 +0530

    - Added authorisation for authentic username from jwt.

--- accountservice/src/main/java/com/tejas/accountservice/services/AccountService.java
+++ accountservice/src/main/java/com/tejas/accountservice/services/AccountService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 import com.tejas.accountservice.models.Account;
@@ -32,7 +33,8 @@ public class AccountService {
 		return new ResponseEntity<>(account, HttpStatus.OK);
 	}
 
-	public ResponseEntity<Account> getAccountByAccountNumber(String accountNumber) {
+	@PreAuthorize("#username == authentication.name or hasName('ADMIN')")
+	public ResponseEntity<Account> getAccountByAccountNumber(String username, String accountNumber) {
 		Account account = repo.getByAccountnumber(accountNumber).get();
 		
 		if (account != null) {
