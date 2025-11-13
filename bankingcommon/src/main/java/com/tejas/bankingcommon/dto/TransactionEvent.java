1bddfab8caef5f2a4fa7b762b53b3be976b43597
kskcoder <tejaskashid21@gmail.com>
2025-11-14 01:59:39 +0530

    - Added userId in event for authentication.

--- bankingcommon/src/main/java/com/tejas/bankingcommon/dto/TransactionEvent.java
+++ bankingcommon/src/main/java/com/tejas/bankingcommon/dto/TransactionEvent.java
@@ -5,6 +5,7 @@ import lombok.Data;
 @Data
 public class TransactionEvent {
     private long transactionId;
+    private int userId;
     private String fromAccountNumber;
     private String toAccountNumber;
     private double amount;
