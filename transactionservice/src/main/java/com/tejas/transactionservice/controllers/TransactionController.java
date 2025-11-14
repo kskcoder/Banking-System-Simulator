d3ba65d307eb88e3bb8ae5875099cd5168269652
kskcoder <tejaskashid21@gmail.com>
2025-11-15 01:51:58 +0530

    - Modified request parameters to path variables in get transactions.

--- transactionservice/src/main/java/com/tejas/transactionservice/controllers/TransactionController.java
+++ transactionservice/src/main/java/com/tejas/transactionservice/controllers/TransactionController.java
@@ -5,10 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tejas.transactionservice.models.Transaction;
@@ -27,22 +27,22 @@ public class TransactionController {
 	}
 	
 	@GetMapping("/transaction/{txnId}")
-	public ResponseEntity<Transaction> getTransaction(@RequestParam int txnId) {
+	public ResponseEntity<Transaction> getTransaction(@PathVariable int txnId) {
 		return accountService.getTransaction(txnId);
 	}
 	
 	@GetMapping("/debit/{accountNumber}")
-	public ResponseEntity<List<Transaction>> getDebitTransaction(@RequestParam String accountNumber) {
+	public ResponseEntity<List<Transaction>> getDebitTransaction(@PathVariable String accountNumber) {
 		return accountService.getDebitTransaction(accountNumber);
 	}
 	
 	@GetMapping("/credit/{accountNumber}")
-	public ResponseEntity<List<Transaction>> getCreditTransaction(@RequestParam String accountNumber) {
+	public ResponseEntity<List<Transaction>> getCreditTransaction(@PathVariable String accountNumber) {
 		return accountService.getCreditTransaction(accountNumber);
 	}
 	
 	@GetMapping("/{accountNumber}")
-	public ResponseEntity<List<Transaction>> getAllTransaction(@RequestParam String accountNumber) {
+	public ResponseEntity<List<Transaction>> getAllTransaction(@PathVariable String accountNumber) {
 		return accountService.getAllTransaction(accountNumber);
 	}
 }
