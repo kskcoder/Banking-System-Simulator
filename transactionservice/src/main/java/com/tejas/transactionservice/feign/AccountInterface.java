2e5ec2d3bcf73b8ff008a87e11cecfbf62b5a543
kskcoder <tejaskashid21@gmail.com>
2025-11-25 13:57:00 +0530

    - Modified feign interface.

--- transactionservice/src/main/java/com/tejas/transactionservice/feign/AccountInterface.java
+++ transactionservice/src/main/java/com/tejas/transactionservice/feign/AccountInterface.java
@@ -1,7 +1,5 @@
 package com.tejas.transactionservice.feign;
 
-import java.util.List;
-
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +9,4 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface AccountInterface {
 	@GetMapping("accounts/accountnumber/{accountNumber}/is-owner")
 	public ResponseEntity<Boolean> isOwnerOfAccountNumber(@PathVariable String accountNumber);
-	
-	@GetMapping("accounts/accountIds")
-	public ResponseEntity<List<Long>> getAccountIdsByUserId();
 }
