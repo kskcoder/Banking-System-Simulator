dac269df98d64047e5e252be9611f0f48db2c986
kskcoder <tejaskashid21@gmail.com>
2025-11-30 14:44:11 +0530

    - Added card verification dto in common project.

---
+++ bankingcommon/src/main/java/com/tejas/bankingcommon/dto/CardVerificationDTO.java
@@ -0,0 +1,16 @@
+package com.tejas.bankingcommon.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class CardVerificationDTO {
+	@NotNull
+	private String cardNumber;
+	@NotNull
+	private String cvv;
+	@NotNull
+	private String expiryDate;
+}
--- bankingcommon/src/main/java/com/tejas/bankingcommon/enums/PaymentStatus.java
+++ bankingcommon/src/main/java/com/tejas/bankingcommon/enums/PaymentStatus.java
@@ -2,6 +2,7 @@ package com.tejas.bankingcommon.enums;
 
 public enum PaymentStatus {
 	INITIATED,
+	OTP_SENT,
 	FAILED,
 	SUCCESS
 }
