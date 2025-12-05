a8bee9bfd00a13f0eb08493a4b2552975fca8d53
kskcoder <tejaskashid21@gmail.com>
2025-12-06 00:55:40 +0530

    - Modified submit otp functions for payment.

--- bankpaymentservice/src/main/java/com/tejas/bankpaymentservice/feign/AuthInterface.java
+++ bankpaymentservice/src/main/java/com/tejas/bankpaymentservice/feign/AuthInterface.java
@@ -6,11 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.tejas.bankingcommon.dto.OtpRequestDTO;
+import com.tejas.bankingcommon.dto.OtpValidateRequest;
+import com.tejas.bankingcommon.dto.OtpValidateResponse;
 
 import jakarta.validation.Valid;
 
 @FeignClient("auth-service")
-public interface UserInterface {
+public interface AuthInterface {
 	@GetMapping("auth/sendotp/{userId}")
 	public ResponseEntity<Boolean> sendPaymentOtp(@Valid @RequestBody OtpRequestDTO request); 
+	
+	@GetMapping("auth/submitotp")
+	public ResponseEntity<OtpValidateResponse> validateOtp(@Valid @RequestBody OtpValidateRequest request); 
 }
