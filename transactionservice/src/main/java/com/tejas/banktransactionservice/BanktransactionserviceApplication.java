d9e2720d7c6965d0e057634fec4cdcc57b9a9581
kskcoder <tejaskashid21@gmail.com>
2025-12-23 01:38:28 +0530

    - Added swagger api gateway depedency.

--- transactionservice/src/main/java/com/tejas/banktransactionservice/BanktransactionserviceApplication.java
+++ transactionservice/src/main/java/com/tejas/banktransactionservice/BanktransactionserviceApplication.java
@@ -6,10 +6,19 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableCaching
+@SecurityScheme(
+	    name = "bearerAuth",
+	    type = SecuritySchemeType.HTTP,
+	    scheme = "bearer",
+	    bearerFormat = "JWT"
+	)
 public class BanktransactionserviceApplication {
 
 	public static void main(String[] args) {
