a81f4170411906dd6b7cd2539dc7e2bda7acbb06
kskcoder <tejaskashid21@gmail.com>
2025-12-23 13:36:41 +0530

    - Enabled feign client in main class file.

--- accountservice/src/main/java/com/tejas/bankaccountservice/BankaccountserviceApplication.java
+++ accountservice/src/main/java/com/tejas/bankaccountservice/BankaccountserviceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
@@ -11,6 +12,7 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 @EnableScheduling
 @EnableCaching
 @SecurityScheme(
