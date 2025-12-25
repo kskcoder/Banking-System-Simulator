73f9fa55ad5e71bc4ed8a18d50435771699a6b5f
kskcoder <tejaskashid21@gmail.com>
2025-12-25 14:17:40 +0530

    - Added open api configuration base url.

---
+++ authservice/src/main/java/com/tejas/bankauthservice/configurations/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package com.tejas.bankauthservice.configurations;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@Configuration
+@OpenAPIDefinition(
+        servers = {
+                @Server(
+                		url = "/bankauthservice", 
+                		description = "API Gateway")
+        }
+)
+public class OpenApiConfig {
+
+}
