db98c553aab89e439d92f47461d42af398072a41
kskcoder <tejaskashid21@gmail.com>
2026-01-03 16:10:33 +0530

    - Renamed Swagger service title.

--- bankpaymentservice/src/main/java/com/tejas/bankpaymentservice/configurations/OpenApiConfig.java
+++ bankpaymentservice/src/main/java/com/tejas/bankpaymentservice/configurations/OpenApiConfig.java
@@ -10,7 +10,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
         servers = {
                 @Server(
                 		url = "/bankpaymentservice", 
-                		description = "API Gateway")
+                		description = "Payment Service")
         }
 )
 public class OpenApiConfig {
