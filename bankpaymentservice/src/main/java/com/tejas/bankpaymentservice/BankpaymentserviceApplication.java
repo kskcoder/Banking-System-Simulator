82306ce141238d1b3fd89a0111338bd3bfce2e5d
kskcoder <tejaskashid21@gmail.com>
2026-01-03 20:47:48 +0530

    - Added caching dependency and annotation.

--- bankpaymentservice/src/main/java/com/tejas/bankpaymentservice/BankpaymentserviceApplication.java
+++ bankpaymentservice/src/main/java/com/tejas/bankpaymentservice/BankpaymentserviceApplication.java
@@ -2,6 +2,7 @@ package com.tejas.bankpaymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
@@ -11,6 +12,7 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
+@EnableCaching
 @SecurityScheme(
 	    name = "bearerAuth",
 	    type = SecuritySchemeType.HTTP,
