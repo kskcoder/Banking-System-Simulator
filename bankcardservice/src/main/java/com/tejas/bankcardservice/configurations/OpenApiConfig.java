56bd3be0c29b600b10a47787ad2fa5a5357e1fbe
kskcoder <tejaskashid21@gmail.com>
2026-01-05 19:37:01 +0530

    - Added title to Payment service swagger page documentation.

--- bankcardservice/src/main/java/com/tejas/bankcardservice/configurations/OpenApiConfig.java
+++ bankcardservice/src/main/java/com/tejas/bankcardservice/configurations/OpenApiConfig.java
@@ -3,10 +3,16 @@ package com.tejas.bankcardservice.configurations;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 @Configuration
 @OpenAPIDefinition(
+        info = @Info(
+                title = "Card Service API",
+                description = "API documentation for Card Service",
+                version = "1.0.0"
+        ),
         servers = {
                 @Server(
                 		url = "/bankcardservice", 
