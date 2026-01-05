9ebb36de63a51199a90c11c925868458ad3746ab
kskcoder <tejaskashid21@gmail.com>
2026-01-05 19:35:31 +0530

    - Added title to Auth swagger page documentation.

--- authservice/src/main/java/com/tejas/bankauthservice/configurations/OpenApiConfig.java
+++ authservice/src/main/java/com/tejas/bankauthservice/configurations/OpenApiConfig.java
@@ -3,10 +3,16 @@ package com.tejas.bankauthservice.configurations;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 @Configuration
 @OpenAPIDefinition(
+        info = @Info(
+                title = "Auth Service API",
+                description = "API documentation for Authorization Service",
+                version = "1.0.0"
+        ),
         servers = {
                 @Server(
                 		url = "/bankauthservice", 
