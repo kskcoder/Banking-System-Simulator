74ab45babd9fa17a31b929dcc0ce3b98f0b7f8e5
kskcoder <tejaskashid21@gmail.com>
2026-01-06 02:18:52 +0530

    - Modified Payment service for inter service calls.

--- bankpaymentservice/src/main/java/com/tejas/bankpaymentservice/feign/FeignInterceptor.java
+++ bankpaymentservice/src/main/java/com/tejas/bankpaymentservice/feign/FeignInterceptor.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import com.tejas.bankingcommon.enums.UserType;
+
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,6 +15,9 @@ import jakarta.servlet.http.HttpServletRequest;
 public class FeignInterceptor implements RequestInterceptor {
 	@Value("${paymentInternalSecretKey}")
 	private String paymentInternalSecretKey;
+	
+	@Value("${interServiceSecretKey}")
+	private String interServiceSecretKey;
 
 	@Override
 	public void apply(RequestTemplate template) {
@@ -23,12 +28,25 @@ public class FeignInterceptor implements RequestInterceptor {
         	
         	String external = request.getHeader("X-External");
         	
-        	if (external.equals("1")) {
+        	if (external != null && external.equals("1")) {
+        		// External payment - use paymentInternalSecretKey
         		template.header("X-Internal-Auth", paymentInternalSecretKey);
         	} else {
         		String authorization = request.getHeader("Authorization");
-        		template.header("Authorization", authorization);
+        		if (authorization != null) {
+        			template.header("Authorization", authorization);
+        		} else {
+        			// No Authorization header available - use inter-service key
+        			template.header("X-Internal-Auth", interServiceSecretKey);
+        			template.header("X-User-Role", UserType.INTERNAL_SERVICE.toString());
+        			template.header("X-User-Id", "INTERNAL_PAYMENT_SERVICE");
+        		}
         	}
+        } else {
+        	// Called from Kafka listener or other non-HTTP context
+        	template.header("X-Internal-Auth", interServiceSecretKey);
+        	template.header("X-User-Role", UserType.INTERNAL_SERVICE.toString());
+        	template.header("X-User-Id", "INTERNAL_PAYMENT_SERVICE");
         }
 	}
 
