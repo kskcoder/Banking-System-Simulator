8089185fe4bd68c3fecafef4988d50ab0b04cba8
kskcoder <tejaskashid21@gmail.com>
2026-01-05 19:36:16 +0530

    - Added title to Account service swagger page documentation.

--- accountservice/src/main/java/com/tejas/bankaccountservice/configurations/OpenApiConfig.java
+++ accountservice/src/main/java/com/tejas/bankaccountservice/configurations/OpenApiConfig.java
@@ -3,10 +3,16 @@ package com.tejas.bankaccountservice.configurations;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 @Configuration
 @OpenAPIDefinition(
+        info = @Info(
+                title = "Account Service API",
+                description = "API documentation for Account Service",
+                version = "1.0.0"
+        ),
         servers = {
                 @Server(
                 		url = "/bankaccountservice", 
