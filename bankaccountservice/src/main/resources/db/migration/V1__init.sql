1b9da32ce62d2e8df8d7841cc5deba83588a0a39
kskcoder <tejaskashid21@gmail.com>
2026-01-15 18:36:37 +0530

    - Added flyway for bootstrapping data into bankcarddb.

--- bankaccountservice/src/main/resources/db/migration/V1__init.sql
+++ bankaccountservice/src/main/resources/db/migration/V1__init.sql
@@ -1,8 +1,9 @@
--- ACCOUNT TABLE
+CREATE TYPE account_type AS ENUM ('SAVINGS', 'CURRENT');
+
 CREATE TABLE accounts (
     id BIGSERIAL PRIMARY KEY,
     userid BIGINT NOT NULL,
     accountnumber VARCHAR(64) NOT NULL UNIQUE,
-    accounttype VARCHAR(20) NOT NULL,
+    accounttype account_type NOT NULL,
     balance DOUBLE PRECISION NOT NULL
 );
---
+++ bankcardservice/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,14 @@
+CREATE TYPE account_card_status AS ENUM ('ACTIVE', 'INACTIVE', 'BLOCKED');
+
+CREATE TABLE cards (
+    id BIGSERIAL PRIMARY KEY,
+    card_number VARCHAR(255) NOT NULL UNIQUE,
+    cvv VARCHAR(255) NOT NULL,
+    last_digits VARCHAR(4) NOT NULL,
+    expiry_date VARCHAR(7) NOT NULL,
+    card_limit DOUBLE PRECISION NOT NULL,
+    status account_card_status NOT NULL,
+    account_id BIGINT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
